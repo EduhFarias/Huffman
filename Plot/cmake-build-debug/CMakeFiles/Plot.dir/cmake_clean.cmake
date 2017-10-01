c714528ae05a0957245e913b9e5788317a2b5356
Eduardo Henrique Farias Silva <ehfs@ic.ufal.br>
2017-10-01 00:10:02 -0300

    Add files via upload

---
+++ Plot/cmake-build-debug/CMakeFiles/Plot.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Plot.dir/main.c.obj"
+  "Plot.pdb"
+  "Plot.exe"
+  "Plot.exe.manifest"
+  "libPlot.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Plot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
