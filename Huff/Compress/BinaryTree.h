726aed46ff277605918f298d53704138b2788927
João Correia <jlmc@ic.ufl.br>
2017-10-03 11:32:17 -0300

    Update Huff

---
+++ Huff/Compress/BinaryTree.h
@@ -0,0 +1,8 @@
+//
+// Created by alunoic on 03/10/17.
+//
+
+#ifndef PROJECT_BINARYTREE_H
+#define PROJECT_BINARYTREE_H
+
+#endif //PROJECT_BINARYTREE_H
---
+++ Huff/Compress/PQueue.h
@@ -0,0 +1,8 @@
+//
+// Created by alunoic on 03/10/17.
+//
+
+#ifndef PROJECT_PQUEUE_H
+#define PROJECT_PQUEUE_H
+
+#endif //PROJECT_PQUEUE_H
