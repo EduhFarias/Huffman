96bf56ef130b524ee35c648f7130b17f011fa668
Eduardo Henrique Farias Silva <ehfs@ic.ufal.br>
2017-10-08 20:25:13 -0300

    Update Huff

--- Huff/Huff.c
+++ Huff/Huff.c
@@ -8,16 +8,17 @@
 #include <stdlib.h>
 
 void compress(FILE *file){
-    int c, i;
+    int c, i, aux = 0;
     BinaryTree *bt = createEmpty();
     int ch[256] = {0};
+
     while((c = fgetc(file)) != EOF){
         ch[c]++;
     }
     for(i = 0; i < 256; i++){
         if(ch[i] > 0){
             printf("%c %d\n", i, ch[i]);
-            bt = createBinaryTree(i,ch[i],bt);
+            bt = createBinaryTree((unsigned char)i,ch[i],bt);
         }
     }
     printx(bt);
