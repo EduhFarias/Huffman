aec6afe1aae782f59e837a043a2a5e379faf0858
Eduardo Henrique Farias Silva <ehfs@ic.ufal.br>
2017-10-26 11:58:26 -0300

    Update Decompress.h

--- Huff/Decompress.h
+++ Huff/Decompress.h
@@ -5,6 +5,10 @@
 #ifndef HUFFMAN_DECOMPRESS_H
 #define HUFFMAN_DECOMPRESS_H
 
+#include "BinaryTree.h"
+
 void decompress(FILE *iFile);
+
+BinaryTree* rebuildTree(BinaryTree *bt, char *tree, int size, int pos);
 
 #endif //HUFFMAN_DECOMPRESS_H
