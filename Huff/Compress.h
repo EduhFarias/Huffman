f25fe4426490b86ea18aeb702b382fd9c1f37a9d
Eduardo Henrique Farias Silva <ehfs@ic.ufal.br>
2017-11-02 18:07:53 -0300

    Update Huff

--- Huff/Compress.h
+++ Huff/Compress.h
@@ -8,21 +8,50 @@
 #include <stdio.h>
 #include "BinaryTree.h"
 
+/*
+ * Recebe o arquivo de entrada e o arquivo de saída
+ * e gera o arquivo compactado.
+*/
 void compress(FILE *iFile, FILE *oFile);
 
-void createTable(BinaryTree *bt, unsigned char table[][256], int pos, unsigned char *aux);
+/*
+ * Recebe um nó raiz de uma árvore, uma posição inicial, um vetor
+ * bidimensional e um vetor, ambos do tipo caractere. Cria a tabela
+ * de conversão a partir da sequência da árvore.
+ */
+void createTable(BinaryTree *bt, char table[][256], int pos, char *aux);
 
+// Monta a árvore de Huffman a partir da fila de prioridades recebida.
 BinaryTree* huff(BinaryTree *bt);
 
+// Calcula a frequência de cada caractere contido no arquivo de entrada.
 void frequency(FILE *iFile, long long int *ch);
 
+/*
+ * Recebe uma lista de frequência e um nó cabeça de uma fila de prioridade.
+ * Para cada posição da lista de frequência é criado um novo nó e adicionado
+ * adequeadamente na lista de prioridade.
+ */
 BinaryTree* createNode(long long int *ch, BinaryTree *bt);
+/*
+ * Recebe o arquivo de entrada e a tabela de conversão, calcula  e retorna
+ * o tamanho do lixo do último byte que será impresso no arquivo de saída.
+ */
+int sizeTrash(FILE *iFile, char table[][256]);
 
-int sizeTrash(FILE *iFile, unsigned char table[][256]);
-
+/*
+ * Recebe o arquivo de saída, o tamanho do lixo e o nó raiz da árvore de Huffman
+ * e imprime no arquivo de saída dois bytes, referentes ao tamanho de lixo e o
+ * tamanho da árvore.
+ */
 void writeTrash_sizeTree(FILE *oFile, int sizeTrash, BinaryTree *bt);
 
-void converterComp(FILE *iFile, FILE *oFile, unsigned char table[][256]);
+/*
+ * Recebe o arquivo de entrada e o arquivo de saída, e a tabela de conversão.
+ * Imprime no arquivo de saída a combinação formada pela tabela de conversão
+ * de acordo com o conteúdo do arquivo de entrada.
+ */
+void converterComp(FILE *iFile, FILE *oFile, char table[][256]);
 
 #endif //HUFFMAN_COMPRESS_H
 
--- Huff/Helpful.h
+++ Huff/Helpful.h
@@ -5,14 +5,28 @@
 #ifndef HUFFMAN_HELPFUL_H
 #define HUFFMAN_HELPFUL_H
 
+// Recebe um byte e uma posição e checa se nessa posição
+// o byte está setado.
 int isBit_i_set(unsigned char c, int i);
 
+// Recebe um byte e uma posição e seta essa posição.
 unsigned char setBit(unsigned char c, int i);
 
-void initialize(unsigned char table[][256], unsigned char *aux);
+// Recebe um vetor bidimensional e um vetor e inicializa-os.
+void initialize(char table[][256], char *aux);
 
+/*
+ * Recebe um item, um vetor de inteiros vazio e seu tamanho.
+ * Conveter o item em seu representante em binário e o
+ * armazena no vetor.
+ */
 void bin_converter(int item, int size, int *aux);
 
+/*
+ * Recebe um vetor de inteiro e seu tamanho, contendo uma
+ * sequência em binário, e converte essa sequência no seu
+ * representante em decimal.
+ */
 int dec_converter(int *aux, int size);
 
 #endif //HUFFMAN_HELPFUL_H
